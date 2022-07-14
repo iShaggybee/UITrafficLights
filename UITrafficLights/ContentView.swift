98a6bf83e845fc4ab309b5f3921b98fc4362c1ba
Vadim Kislov <kislov.v@gmail.com>
2022-07-14 08:43:38 +0300

    Initial Commit

---
+++ UITrafficLights/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UITrafficLights
+//
+//  Created by Kislov Vadim on 14.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UITrafficLights/UITrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITrafficLightsApp.swift
+//  UITrafficLights
+//
+//  Created by Kislov Vadim on 14.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct UITrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
